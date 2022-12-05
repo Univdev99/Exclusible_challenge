9d478675852d0e033dd4a92302c626a58c10461f
Univdev <univdev99@gmail.com>
2022-12-06 00:34:55 +0800

    Configure project

---
+++ .solcover.js
@@ -0,0 +1,11 @@
+const skipFiles = []
+
+module.exports = {
+//   providerOptions: {
+//     account: process.env.privatekey,
+//     network_id: 5,
+//   },
+  skipFiles,
+  istanbulFolder: './reports/coverage',
+  testfiles: 'test/*.test.ts'
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,87 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-etherscan'
+import '@nomiclabs/hardhat-waffle'
+import 'hardhat-abi-exporter'
+import 'hardhat-gas-reporter'
+import 'hardhat-contract-sizer'
+import '@tenderly/hardhat-tenderly'
+import '@openzeppelin/hardhat-upgrades'
+import '@typechain/hardhat'
+import 'solidity-coverage'
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    artifacts: './build/contracts',
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+          outputSelection: {
+            '*': {
+              '*': ['storageLayout'],
+            },
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
+      chainId: 5,
+      accounts: [process.env.privatekey]
+    },
+    ganache: {
+      chainId: 1337,
+      url: 'http://localhost:8545'
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false,
+    showTimeSpent: true,
+    currency: 'USD',
+    outputFile: 'reports/gas-report.log',
+  },
+  typechain: {
+    outDir: 'build/types',
+    target: 'ethers-v5',
+  },
+  abiExporter: {
+    path: './build/abis',
+    clear: false,
+    flat: true,
+  },
+  tenderly: {
+    project: process.env.TENDERLY_PROJECT,
+    username: process.env.TENDERLY_USERNAME,
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: false,
+    disambiguatePaths: true,
+  },
+}
